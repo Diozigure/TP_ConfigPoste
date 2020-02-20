5bdee5c380b05a7f5ae61345c673e7475b36a78d
berti <berti@DESKTOP-NBS4OS4>
2020-02-20 11:57:09 +0100

    test

--- src/main/java/Create.sql
+++ src/main/java/Create.sql
@@ -5,7 +5,7 @@ DESCRIPTION VARCHAR2(100)
 );
 
 INSERT INTO Recette (ID, NAME, DESCRIPTION) VALUES (1, "Tarte aux pommes", "Une délicieuse tarte aux pommes");
-INSERT INTO Recette (ID, NAME, DESCRIPTION) VALUES (1, "Tarte aux abricots", "Une délicieuse tarte aux abricots");
-INSERT INTO Recette (ID, NAME, DESCRIPTION) VALUES (1, "Tarte aux fraises", "Une délicieuse tarte aux fraises");
+INSERT INTO Recette (ID, NAME, DESCRIPTION) VALUES (2, "Tarte aux abricots", "Une délicieuse tarte aux abricots");
+INSERT INTO Recette (ID, NAME, DESCRIPTION) VALUES (3, "Tarte aux fraises", "Une délicieuse tarte aux fraises");
 
 commit;
--- src/main/java/epsi/cooking/utils/DataUtils.java
+++ src/main/java/epsi/cooking/utils/DataUtils.java
@@ -1,5 +1,6 @@
 package epsi.cooking.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -9,20 +10,30 @@ import org.hibernate.query.Query;
 import epsi.cooking.entities.Recette;
 
 public class DataUtils {
+	// public static List<Recette> getRecettes() {
+	// List<Recette> lRecette = null;
+	// SessionFactory factory = HibernateUtils.getSessionFactory();
+	// Session session = factory.getCurrentSession();
+	// try {
+	// session.getTransaction().begin();
+	// String sql = "Select r from Recette r";
+	// Query<Recette> query = session.createQuery(sql);
+	//
+	// lRecette = query.getResultList();
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// session.getTransaction().rollback();
+	// }
+	// return lRecette;
+	// }
 	public static List<Recette> getRecettes() {
-		List<Recette> lRecette = null;
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.getCurrentSession();
-		try {
-			session.getTransaction().begin();
-			String sql = "Select r from Recette r";
-			Query<Recette> query = session.createQuery(sql);
-
-			lRecette = query.getResultList();
-		} catch (Exception e) {
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		}
+		List<Recette> lRecette = new ArrayList<Recette>();
+		Recette r1 = new Recette(1, "Tarte aux pommes", "Une délicieuse tarte aux pommes");
+		Recette r2 = new Recette(2, "Tarte aux abricots", "Une délicieuse tarte aux abricots");
+		Recette r3 = new Recette(3, "Tarte aux fraises", "Une délicieuse tarte aux fraises");
+		lRecette.add(r1);
+		lRecette.add(r2);
+		lRecette.add(r3);
 		return lRecette;
 	}
 }
